977fbd8b2c2d2c6746991d174b0377ed13476885
alexjball (via Travis CI) <8595776+alexjball@users.noreply.github.com>
2020-04-20 00:34:17 +0000

    Deploy alexjball/harm-reduction-frontend to github.com/alexjball/harm-reduction-frontend.git:gh-pages

---
+++ precache-manifest.9db2661af207e0b9609232c9987e5818.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b88fb43cd7fa3120afcf8ba791bcb0f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d9e1c6243abe43180b9b",
+    "url": "/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "3f8586649ae16609805d",
+    "url": "/static/css/main.25f5029a.chunk.css"
+  },
+  {
+    "revision": "d9e1c6243abe43180b9b",
+    "url": "/static/js/2.f45b5583.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.f45b5583.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f8586649ae16609805d",
+    "url": "/static/js/main.cfb7cedd.chunk.js"
+  },
+  {
+    "revision": "82370b46b8bf571cff7f",
+    "url": "/static/js/runtime-main.e030e367.js"
+  }
+]);
--- src/widgets.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-
-export const P = ({ children }) => (
-  <p style={{ fontWeight: 500 }}>{children}</p>
-);
